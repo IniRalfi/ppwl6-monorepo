7cd4c33bf02cb6f210de1ea8c6d744d9928060aa
Rafli Pratama <rflipratm@gmail.com>
2026-03-17 14:45:15 +0700

    update

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+// import App from "./App";
+// import App from "./ApiTest";
 import App from "./App2";
 
 createRoot(document.getElementById("root")!).render(
